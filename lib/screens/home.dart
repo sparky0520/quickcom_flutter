e9d5e1ab0b9e13c48b0c14940e6a79f85bb7a4d0
Darsh Jain <132695189+sparky0520@users.noreply.github.com>
2025-06-10 23:30:00 +0530

    checkp before http

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -9,56 +9,86 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final List<Product> _products = [
-    Product(
-      platform: "Blinkit",
-      price: 12,
-      quantity: "quantity",
-      title: "title",
-    ),
-    Product(
-      platform: "Blinkit",
-      price: 12,
-      quantity: "quantity",
-      title: "title",
-    ),
-    Product(
-      platform: "Blinkit",
-      price: 12,
-      quantity: "quantity",
-      title: "title",
-    ),
-  ];
+  final _queryController = TextEditingController();
+  final List<Product> _products = [];
+
+  void _fetchResults(String query) async {
+    // await
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-        ),
-        itemCount: _products.length,
-        itemBuilder: (ctx, index) {
-          return GridTile(
-            child: Column(
-              spacing: 12,
-              children: [
-                Text(_products[index].platform),
+      appBar: AppBar(title: Text("QuickCom")),
+      body: Container(
+        color: Colors.black12,
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            spacing: 12,
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _queryController,
+                      decoration: InputDecoration(
+                        label: Text("Type the product you want to search"),
+                      ),
+                    ),
+                  ),
+                  TextButton.icon(
+                    onPressed: () {
+                      final t = _queryController.text.trim();
+                      if (t.isNotEmpty) {
+                        _fetchResults(t);
+                      }
+                    },
+                    icon: Icon(Icons.search),
+                    label: Text("Search"),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 12,
+                    mainAxisSpacing: 12,
+                  ),
+                  itemCount: _products.length,
+                  itemBuilder: (ctx, index) {
+                    return Container(
+                      padding: const EdgeInsets.all(12),
+                      decoration: BoxDecoration(
+                        border: Border.all(width: 1, color: Colors.black38),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
 
-                if (_products[index].imageUrl != null)
-                  Image.network(_products[index].imageUrl!),
+                        children: [
+                          Text(_products[index].platform),
 
-                Text(_products[index].title),
-                Text(_products[index].quantity),
-                Text("₹${_products[index].price}"),
-                TextButton.icon(
-                  onPressed: () {},
-                  icon: Icon(Icons.subdirectory_arrow_right),
-                  label: Text("Buy Now"),
+                          if (_products[index].imageUrl != null)
+                            Image.network(_products[index].imageUrl!),
+
+                          Text(_products[index].title),
+                          Text(_products[index].quantity),
+                          Text("₹${_products[index].price}"),
+                          TextButton.icon(
+                            onPressed: () {},
+                            icon: Icon(Icons.subdirectory_arrow_right),
+                            label: Text("Buy Now"),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
                 ),
-              ],
-            ),
-          );
-        },
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
